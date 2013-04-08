d7404dab68ab3cc5237a8239789a3a85e9e0bcb8
Kevin Brownell <kevin.brownell@gmail.com>
2013-04-08 12:22:41 -0400

    Made bus utilization a float, for prefetcher

--- zesto-noc.cpp
+++ zesto-noc.cpp
@@ -41,7 +41,7 @@ void bus_reg_stats(
   stat_reg_counter(sdb, true, buf, buf2, &bus->stat.accesses, 0, TRUE, NULL);
   sprintf(buf,"%s%s.utilization",core_str,bus->name);
   sprintf(buf2,"cumulative cycles of utilization of bus %s",bus->name);
-  stat_reg_counter(sdb, true, buf, buf2, &bus->stat.utilization, 0, TRUE, NULL);
+  stat_reg_double(sdb, true, buf, buf2, &bus->stat.utilization, 0, TRUE, NULL);
   sprintf(buf,"%s%s.avg_burst",core_str,bus->name);
   sprintf(buf2,"avg cycles utilized per transfer of bus %s",bus->name);
   sprintf(buf3,"%s%s.utilization/%s%s.accesses",core_str,bus->name,core_str,bus->name);
@@ -52,7 +52,7 @@ void bus_reg_stats(
   stat_reg_formula(sdb, true, buf, buf2, buf3, "%12.4f");
   sprintf(buf,"%s%s.pf_utilization",core_str,bus->name);
   sprintf(buf2,"cumulative cycles of utilization of bus %s for prefetches",bus->name);
-  stat_reg_counter(sdb, true, buf, buf2, &bus->stat.prefetch_utilization, 0, TRUE, NULL);
+  stat_reg_double(sdb, true, buf, buf2, &bus->stat.prefetch_utilization, 0, TRUE, NULL);
   sprintf(buf,"%s%s.pf_duty_cycle",core_str,bus->name);
   sprintf(buf2,"fraction of time bus %s was in use for prefetches",bus->name);
   sprintf(buf3,"%s%s.pf_utilization/sim_cycle",core_str,bus->name);
@@ -63,7 +63,7 @@ void bus_reg_stats(
 int bus_free(const struct bus_t * const bus)
 {
   /* HACEDY HACKEDY HACK -- magic FSB */
-  if(fsb_magic)
+  if(fsb_magic && strncmp(bus->name, "FSB", 3) == 0)
     return true;
   /* assume bus clock is locked to cpu clock (synchronous): only
      allow operations when cycle MOD bus-multiplier is zero */
@@ -79,8 +79,8 @@ void bus_use(
     const int transfer_size,
     const int prefetch)
 {
-  const int latency = (((transfer_size-1) / bus->width)+1) * bus->ratio; /* round up */
-  bus->when_available = sim_cycle + latency;
+  const double latency = ((transfer_size) / (double)bus->width) * bus->ratio;
+  bus->when_available = (int)(sim_cycle + latency); /* round down*/
   bus->stat.accesses++;
   bus->stat.utilization += latency;
   if(prefetch)
