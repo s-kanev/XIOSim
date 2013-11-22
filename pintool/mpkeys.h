aea34d3783a41d4d0b476c6b4d6e711103d7997d
Sam Xi <samxi@seas.harvard.edu>
2013-11-22 17:43:29 -0500

    Working global constants.

--- pintool/mpkeys.h
+++ pintool/mpkeys.h
@@ -12,31 +12,33 @@
 namespace xiosim {
 namespace shared {
 
+extern "C" {
+
 // Global shared memory key names.
-const std::string XIOSIM_SHARED_MEMORY_KEY = "xiosim_shared_memory";
-const std::string XIOSIM_INIT_SHARED_LOCK = "xiosim_init_shared_lock";
-const std::string BUFFER_MANAGER_SHARED_MEMORY_KEY = "buffer_manager_shared_memory_key";
-const std::string XIOSIM_INIT_COUNTER_KEY = "xiosim_init_counter";
+extern const char* XIOSIM_SHARED_MEMORY_KEY;
+extern const char* XIOSIM_INIT_SHARED_LOCK;
+extern const char* BUFFER_MANAGER_SHARED_MEMORY_KEY;
+extern const char* XIOSIM_INIT_COUNTER_KEY;
 
 // Shared memory default sizes
-const std::size_t DEFAULT_SHARED_MEMORY_SIZE = 65536;
+extern const std::size_t DEFAULT_SHARED_MEMORY_SIZE;
 
 // BufferManager keys.
-const std::string BUFFER_MANAGER_LOCKS_ = "buffer_manager_locks";
-const std::string BUFFER_MANAGER_POOL_ = "buffer_manager_pool";
-const std::string BUFFER_MANAGER_LOGS_ = "buffer_manager_logs";
-const std::string BUFFER_MANAGER_QUEUE_SIZES_ = "buffer_manager_queue_sizes";
-const std::string BUFFER_MANAGER_FAKE_FILE_ = "buffer_manager_fake_file";
-const std::string BUFFER_MANAGER_CONSUME_BUFFER_ = "buffer_manager_consume_buffer";
-const std::string BUFFER_MANAGER_PRODUCE_BUFFER_ = "buffer_manager_produce_buffer";
-const std::string BUFFER_MANAGER_FILE_ENTRY_COUNT_ = "buffer_manager_file_entry_count";
-const std::string BUFFER_MANAGER_FILE_NAMES_ = "buffer_manager_file_names_";
-const std::string BUFFER_MANAGER_FILE_COUNTS_ = "buffer_manager_file_counts";
-const std::string BUFFER_MANAGER_READ_BUFFER_SIZE_ = "buffer_manager_read_buffer_size";
-const std::string BUFFER_MANAGER_READ_BUFFER_ = "buffer_manager_read_buffer";
-const std::string BUFFER_MANAGER_WRITE_BUFFER_SIZE_ = "buffer_manager_write_buffer";
-const std::string BUFFER_MANAGER_WRITE_BUFFER_ = "buffer_manager_write";
-
+extern const char* BUFFER_MANAGER_LOCKS_;
+extern const char* BUFFER_MANAGER_POOL_;
+extern const char* BUFFER_MANAGER_LOGS_;
+extern const char* BUFFER_MANAGER_QUEUE_SIZES_;
+extern const char* BUFFER_MANAGER_FAKE_FILE_;
+extern const char* BUFFER_MANAGER_CONSUME_BUFFER_;
+extern const char* BUFFER_MANAGER_PRODUCE_BUFFER_;
+extern const char* BUFFER_MANAGER_FILE_ENTRY_COUNT_;
+extern const char* BUFFER_MANAGER_FILE_NAMES_;
+extern const char* BUFFER_MANAGER_FILE_COUNTS_;
+extern const char* BUFFER_MANAGER_READ_BUFFER_SIZE_;
+extern const char* BUFFER_MANAGER_READ_BUFFER_;
+extern const char* BUFFER_MANAGER_WRITE_BUFFER_SIZE_;
+extern const char* BUFFER_MANAGER_WRITE_BUFFER_;
+}
 
 /*
 // Shared memory variable typedefs.
---
+++ pintool/mpkeys_impl.cpp
@@ -0,0 +1,35 @@
+#include "mpkeys.h"
+
+namespace xiosim {
+namespace shared {
+
+extern "C" {
+
+const char *XIOSIM_SHARED_MEMORY_KEY = "xiosim_shared_memory";
+const char *XIOSIM_INIT_SHARED_LOCK = "xiosim_init_shared_lock";
+const char *BUFFER_MANAGER_SHARED_MEMORY_KEY = "buffer_manager_shared_memory_key";
+const char *XIOSIM_INIT_COUNTER_KEY = "xiosim_init_counter";
+
+// Shared memory default sizes
+const std::size_t DEFAULT_SHARED_MEMORY_SIZE = 65536;
+
+// BufferManager keys.
+const char *BUFFER_MANAGER_LOCKS_ = "buffer_manager_locks";
+const char *BUFFER_MANAGER_POOL_ = "buffer_manager_pool";
+const char *BUFFER_MANAGER_LOGS_ = "buffer_manager_logs";
+const char *BUFFER_MANAGER_QUEUE_SIZES_ = "buffer_manager_queue_sizes";
+const char *BUFFER_MANAGER_FAKE_FILE_ = "buffer_manager_fake_file";
+const char *BUFFER_MANAGER_CONSUME_BUFFER_ = "buffer_manager_consume_buffer";
+const char *BUFFER_MANAGER_PRODUCE_BUFFER_ = "buffer_manager_produce_buffer";
+const char *BUFFER_MANAGER_FILE_ENTRY_COUNT_ = "buffer_manager_file_entry_count";
+const char *BUFFER_MANAGER_FILE_NAMES_ = "buffer_manager_file_names_";
+const char *BUFFER_MANAGER_FILE_COUNTS_ = "buffer_manager_file_counts";
+const char *BUFFER_MANAGER_READ_BUFFER_SIZE_ = "buffer_manager_read_buffer_size";
+const char *BUFFER_MANAGER_READ_BUFFER_ = "buffer_manager_read_buffer";
+const char *BUFFER_MANAGER_WRITE_BUFFER_SIZE_ = "buffer_manager_write_buffer";
+const char *BUFFER_MANAGER_WRITE_BUFFER_ = "buffer_manager_write";
+
+}
+
+}
+}
