df4934deacc14b07e816a3b1457746d5d743591c
Sam Xi <samxi@seas.harvard.edu>
2013-12-06 20:41:40 -0500

    Harness now parses a configuration file to determine which benchmarks to run.

--- pintool/harness.cpp
+++ pintool/harness.cpp
@@ -1,18 +1,12 @@
 // Harness utility that launches XIOSIM for a multiprogrammed workload. Muliple
-// Pin processes running a pintool for a workload are forked. The harness
-// creates a shared memory segment for XIOSIM to use as interprocess
-// communication between the separate Pin processes.
+// pintool processes for a workload are forked according to a configuration
+// file. The harness creates a shared memory segment for XIOSIM to use as
+// interprocess communication between the separate Pin processes.
 //
-// To usei:
-//   harness -num_processes n setarch i686 ... pin -pin_args ...
-//     ./pin_tool.o -pin_tool_flags ...
+// To use:
+//   harness -benchmark_cfg <CONFIG_FILE> setarch i686 ... pin -pin_args ...
 //
-// In other words, run the pintool like usual with all of the necessary flags,
-// but pass the entire command string to the harness executable along with a
-// num_processes flag that tells the harness how many instances of Pin to fork.
-//
-// If the num_processes flag is present, it MUST be the first flag passed to the
-// harness. If the flag is absent, it defaults to 1.
+// The benchmarK_cfg flag is required.
 //
 // Author: Sam Xi
 
@@ -35,9 +29,12 @@
 
 #include "mpkeys.h"
 
+#include "confuse.h"  // For parsing config files.
+
 namespace xiosim {
 namespace shared {
 
+const std::string CFG_FILE_FLAG = "-benchmark_cfg";
 const std::string NUM_PROCESSES_FLAG = "-num_processes";
 const std::string LAST_PINTOOL_ARG = "-s";
 
@@ -61,10 +58,12 @@ void kill_handler(int sig) {
 }
 
 std::string get_timing_sim_args(std::string harness_args) {
-    std::string res = boost::replace_all_copy<std::string>(harness_args, "feeder_zesto", "timing_sim");
+    std::string res = boost::replace_all_copy<std::string>(
+        harness_args, "feeder_zesto", "timing_sim");
 
     auto pos = res.rfind("--");
-    res.replace(pos, res.length(), "-- ./timing_wait"); //XXX: get path from feeder_zesto.so
+    //XXX: get path from feeder_zesto.so
+    res.replace(pos, res.length(), "-- ./timing_wait");
     std::cout << "timing_sim args: " << res << std::endl;
     return res;
 }
@@ -78,21 +77,38 @@ int main(int argc, char **argv) {
   using namespace boost::interprocess;
   using namespace xiosim::shared;
 
-  // Get the argument to the num_processes flag if present.
-  harness_num_processes = 1;
-  bool flag_present = false;
-  std::stringstream convert(argv[2]);
-  if (strncmp(NUM_PROCESSES_FLAG.c_str(), argv[1],
-              NUM_PROCESSES_FLAG.length()) == 0) {
-    flag_present = true;
-    if (!(convert >> harness_num_processes)) {
-      std::cerr << "Invalid argument to num_processes flag." << std::endl;
-      exit(-1);
-    }
+  // Get the argument to the cfg_filename flag.
+  std::string cfg_filename = "";
+  if (strncmp(CFG_FILE_FLAG.c_str(), argv[1],
+              CFG_FILE_FLAG.length()) == 0) {
+    cfg_filename = argv[2];
+  } else {
+    std::cerr << "Must specify a valid benchmark configuration file with the "
+             << "-benchmark_cfg flag." << std::endl;
+    exit(1);
   }
 
-  // Create a process for timing sim
-  harness_num_processes++;
+  // Parse the benchmark configuration file.
+  cfg_opt_t program_opts[] {
+      CFG_STR("exec_path", "", CFGF_NONE),
+      CFG_STR("command_line_args", "", CFGF_NONE),
+      CFG_INT("instances", 1, CFGF_NONE),
+      CFG_END()
+  };
+  cfg_opt_t opts[] {
+      CFG_SEC("program", program_opts, CFGF_MULTI | CFGF_TITLE),
+      CFG_END()
+  };
+  cfg_t *cfg = cfg_init(opts, 0);
+  cfg_parse(cfg, cfg_filename.c_str());
+
+  // Compute the total number of benchmark processes that will be forked.
+  int num_programs = cfg_size(cfg, "program");
+  int harness_num_processes = 0;
+  for (int i = 0; i < num_programs; i++) {
+    cfg_t *program_cfg = cfg_getnsec(cfg, "program", i);
+    harness_num_processes += cfg_getint(program_cfg, "instances");
+  }
 
   // Setup SIGINT handler to kill child processes as well.
   struct sigaction sig_int_handler;
@@ -104,7 +120,7 @@ int main(int argc, char **argv) {
   // Concatenate the flags into a single string to be executed when setup is
   // complete. Exclude the num_processes flag part if present.
   std::stringstream command_stream;
-  int command_start_pos = flag_present ? 3 : 1;
+  int command_start_pos = 3;  // There are three harness-specific arguments.
   for (int i = command_start_pos; i < argc; i++) {
     // If the next arg is "-s", add the num_processes flag. We need to tell the
     // pintool how many processes we have running so it can set up shared memory
@@ -112,10 +128,11 @@ int main(int argc, char **argv) {
     if (strncmp(argv[i], LAST_PINTOOL_ARG.c_str(),
                 LAST_PINTOOL_ARG.length()) == 0) {
       command_stream << " " << NUM_PROCESSES_FLAG << " "
-                     << harness_num_processes-1 << " ";
+                     << harness_num_processes << " ";
     }
     command_stream << argv[i] << " ";
   }
+  command_stream << "-- ";  // For appending the benchmark program arguments.
 
   // Removes any shared data left from a previous run if they exist.
   shared_memory_object::remove(XIOSIM_SHARED_MEMORY_KEY);
@@ -125,7 +142,7 @@ int main(int argc, char **argv) {
   permissions perm;
   perm.set_unrestricted();
   managed_shared_memory shm(open_or_create, XIOSIM_SHARED_MEMORY_KEY,
-       DEFAULT_SHARED_MEMORY_SIZE);//, (void*) 0x0, perm);
+       DEFAULT_SHARED_MEMORY_SIZE);
 
   // Sets up a counter for multiprogramming. It is initialized to the number of
   // processes that are to be forked. When each forked process is about to start
@@ -134,23 +151,41 @@ int main(int argc, char **argv) {
   // This also initializes a shared mutex for the forked processes to use for
   // synchronizing access to this counter.
   int *counter =
-    shm.find_or_construct<int>(XIOSIM_INIT_COUNTER_KEY)(harness_num_processes-1);
+    shm.find_or_construct<int>(XIOSIM_INIT_COUNTER_KEY)(harness_num_processes);
   named_mutex init_lock(open_or_create, XIOSIM_INIT_SHARED_LOCK, perm);
   init_lock.unlock();
 
+  // Create a process for timing sim
+  harness_num_processes++;
+
+  // Fork all the child processes.
   int status;
   harness_pids = new pid_t[harness_num_processes];
   for (int i = 0; i < harness_num_processes; i++) {
     harness_pids[i] = fork();
+    std::string run_str = command_stream.str();
+
+    // Append program arguments if we're not starting the timing simulator. Make
+    // sure we're getting the right configuration block (i - 1).
+    if (i != 0) {
+      std::stringstream ss;
+      cfg_t *program_cfg = cfg_getnsec(cfg, "program", i - 1);
+      ss << cfg_getstr(program_cfg, "exec_path") << " " <<
+            cfg_getstr(program_cfg, "command_line_args");
+      run_str += ss.str();
+    }
 
     switch (harness_pids[i]) {
-      case 0:  { // child
+      case 0:  {  // child
         if (i == 0) {
-          std::string timing_cmd = get_timing_sim_args(command_stream.str());
-          system(timing_cmd.c_str()); 
+          // Start the timing simulator.
+          std::string timing_cmd = get_timing_sim_args(run_str);
+          system(timing_cmd.c_str());
         }
-        else
-          system(command_stream.str().c_str());
+        else {
+          std::cout << run_str << std::endl;
+          system(run_str.c_str());
+        }
         exit(0);
         break;
       }
@@ -159,7 +194,10 @@ int main(int argc, char **argv) {
         break;
       }
       default:  {  // parent
-        std::cout << "New process " << harness_pids[i] << std::endl;
+        if (i == 0)
+          std::cout << "Timing simulator: " << harness_pids[i] << std::endl;
+        else
+          std::cout << "New producer: " << harness_pids[i] << std::endl;
         break;
       }
     }
