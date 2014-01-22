81ff2bf06e6e64d37529fb79278189d7341a5fac
Svilen Kanev <skanev@eecs.harvard.edu>
2014-01-22 17:15:31 -0500

    Add -Wno-unused-function globally.

--- pintool/ezOptionParser_clean.hpp
+++ pintool/ezOptionParser_clean.hpp
@@ -1,9 +1,8 @@
-#pragma diagnostic push
+#pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wsign-compare"
 #pragma GCC diagnostic ignored "-Wreorder"
 #pragma GCC diagnostic ignored "-Wparentheses"
 #pragma GCC diagnostic ignored "-Wunused-variable"
-#pragma GCC diagnostic ignored "-Wunused-function"
 #pragma GCC diagnostic ignored "-Wformat-extra-args"
 #include "ezOptionParser.hpp"
-#pragma diagnostic pop
+#pragma GCC diagnostic pop
