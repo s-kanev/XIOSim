209470be05ac62f3b749fa18e1d58184dff057ff
Sam Xi <samxi@seas.harvard.edu>
2014-05-07 21:35:56 -0400

    Implemented gradient descent function to find local minimum around a starting point. So far, this has turned out to give us the correct global minimum. Implemented other related functions that are used in the optimization.
    Added a bunch of documentation.

--- pintool/base_speedup_model.cpp
+++ pintool/base_speedup_model.cpp
@@ -1,55 +1,111 @@
+/* Implementation of BaseSpeedupModel functions common to all speedup models.
+ * Author: Sam Xi.
+ */
+
 #include "base_speedup_model.h"
+
+/* This is a helper convenience method that does some setup before calling the
+ * function that does all the actual minimization work.
+ */
+double BaseSpeedupModel::MinimizeCoreAllocations(
+        std::map<int, int> &core_alloc,
+        std::vector<double> &process_scaling,
+        std::vector<double> &process_serial_runtime) {
+    std::vector<int> process_list;
+    for (auto it = core_alloc.begin(); it != core_alloc.end(); ++it)
+        process_list.push_back(it->first);
+    return MinimizeCoreAllocations(
+            core_alloc,
+            process_scaling,
+            process_serial_runtime,
+            process_list,
+            -1);
+}
 
 /* Performs gradient descent to find the nearest minimum runtime point to a
  * starting core allocation, specified in core_alloc. Since the analytical
- * method is guaranteed to bring us very close to the true solution, it is a
- * safe assumption that any minima near this point is a global minima. This has
- * been verified with extensive testing.
+ * method should bring us very close to the true solution, it is a safe
+ * assumption that any minima near this point is a global minima.
  */
 double BaseSpeedupModel::MinimizeCoreAllocations(
         std::map<int, int> &core_alloc,
         std::vector<double> &process_scaling,
         std::vector<double> &process_serial_runtime,
-        double prev_max) {
-    double current_max = 0;
-    // Compute the current worst case runtime.
+        std::vector<int> &process_list,
+        double previous_max) {
+    double current_min_energy = ComputeEnergy(
+            core_alloc, process_scaling, process_serial_runtime);
+    if (process_list.empty())
+        return current_min_energy;
+
+    std::map<int, int> best_alloc(core_alloc);
+    int num_processes = core_alloc.size();
+
+    std::vector<int> valid_steps;
+    valid_steps.push_back(1);
+    valid_steps.push_back(0);
+    valid_steps.push_back(-1);
+    bool updated = false;
+    do {
+        updated = false;
+        for (size_t i = 0; i < process_list.size(); i++) {
+            for (auto j = valid_steps.begin(); j != valid_steps.end(); ++j) {
+                // Test that the current allocation is at least one and not more
+                // than num_cores - num_processes + 1 (which is the max per process
+                // assuming each process is allocated at least one core). If not so,
+                // then we ignore this allocation.
+                int current_cores_alloc = ComputeCurrentCoresAllocated(best_alloc);
+                std::map<int, int> temp_alloc(best_alloc);
+                int current_proc = process_list[i];
+                if (current_cores_alloc + *j <= num_cores &&
+                    temp_alloc[current_proc] + *j > 0 &&
+                    temp_alloc[current_proc] + *j <= num_cores - num_processes + 1) {
+                    temp_alloc[current_proc] += *j;
+                    std::vector<int> next_process_list(process_list);
+                    next_process_list.erase(next_process_list.begin() + i);
+                    double temp_energy = MinimizeCoreAllocations(
+                        temp_alloc, process_scaling, process_serial_runtime,
+                        next_process_list, current_min_energy);
+                    if (temp_energy < current_min_energy) {
+                        current_min_energy = temp_energy;
+                        best_alloc = temp_alloc;
+                        updated = true;
+                    }
+                }
+            }
+        }
+    } while (updated);
+
+    core_alloc = best_alloc;
+    return current_min_energy;
+}
+
+/* Calls the implemented ComputeRuntime() function in child classes to compute
+ * energy.
+ */
+double BaseSpeedupModel::ComputeEnergy(
+        std::map<int, int> &core_alloc,
+        std::vector<double> &process_scaling,
+        std::vector<double> &process_serial_runtime) {
+    double current_energy = 0;
+    double current_max_rt = 0;
     for (auto it = core_alloc.begin(); it != core_alloc.end(); ++it) {
         double runtime = ComputeRuntime(
                 it->second,
                 process_scaling[it->first],
                 process_serial_runtime[it->first]);
-        if (runtime > current_max)
-            current_max = runtime;
+        current_energy += core_power * runtime * it->second;
+        if (runtime > current_max_rt)
+            current_max_rt = runtime;
     }
-    // If the current max runtime is larger than the previous maximum (from the
-    // calling function), then this allocation is less optimal.
-    if (prev_max != -1 && current_max >= prev_max)
-        return -1;
+    current_energy += uncore_power * current_max_rt;
+    return current_energy;
+}
 
-    // This allocation is more optimal than the previous one. Continue.
-    // TODO: This might not work for log scaling where the total number of
-    // allocated cores can change.
-    std::map<int, int> best_alloc(core_alloc);
-    for (auto i = core_alloc.begin(); i != core_alloc.end(); ++i) {
-        core_alloc[i->first] ++;
-        for (auto j = core_alloc.begin(); j != core_alloc.end(); ++j) {
-            if (i->first != j->first && j->second > 1) {
-                core_alloc[j->first] --;
-                double next_max = MinimizeCoreAllocations(
-                        core_alloc,
-                        process_scaling,
-                        process_serial_runtime,
-                        current_max);
-                if (next_max != -1 && next_max < current_max) {
-                    best_alloc = core_alloc;
-                    current_max = next_max;
-                } else {
-                    core_alloc[j->first] ++;
-                }
-            }
-        }
-        core_alloc[i->first] --;
+int BaseSpeedupModel::ComputeCurrentCoresAllocated(std::map<int, int> &core_alloc) {
+    int sum = 0;
+    for (auto it = core_alloc.begin(); it != core_alloc.end(); ++it) {
+        sum += it->second;
     }
-    core_alloc = best_alloc;
-    return current_max;
+    return sum;
 }
--- pintool/base_speedup_model.h
+++ pintool/base_speedup_model.h
@@ -18,9 +18,10 @@ class BaseSpeedupModel {
         /* Sets the core power and uncore power values, which are needed to
          * compute energy.
          */
-        BaseSpeedupModel(double core_power, double uncore_power) {
+        BaseSpeedupModel(double core_power, double uncore_power, int num_cores) {
             this->core_power = core_power;
             this->uncore_power = uncore_power;
+            this->num_cores = num_cores;
         }
 
         /* Returns a core allocation that minimizes total energy consumption for
@@ -41,8 +42,21 @@ class BaseSpeedupModel {
         virtual void OptimizeEnergy(
                 std::map<int, int> &core_allocs,
                 std::vector<double> &process_scaling,
-                std::vector<double> &process_serial_runtime,
-                int num_cores) = 0;
+                std::vector<double> &process_serial_runtime) = 0;
+
+    protected:
+        /* The power consumed by a core. Assume that core power is constant
+         * regardless of utilization.
+         */
+        double core_power;
+
+        /* The power consumed by uncore units, such as shared last-level caches,
+         * memory controllers, power management systems, and IO.
+         */
+        double uncore_power;
+
+        /* The total number of cores in the system. */
+        int num_cores;
 
         /* Computes parallel runtime of a process given the number of allocated
          * cores and scaling data.
@@ -61,25 +75,16 @@ class BaseSpeedupModel {
                 int num_cores_alloc,
                 double process_scaling,
                 double process_serial_runtime) = 0;
-    protected:
-        /* The power consumed by a core. Assume that core power is constant
-         * regardless of utilization.
-         */
-        double core_power;
-
-        /* The power consumed by uncore units, such as shared last-level caches,
-         * memory controllers, power management systems, and IO.
-         */
-        double uncore_power;
 
         /* Performs gradient descent to find the nearest minimum runtime point
          * to a starting core allocation, specified in core_alloc. Since the
          * analytical method is guaranteed to bring us very close to the true
-         * solution, it is a safe assumption that any minima near this point is
-         * a global minima. This has been verified with extensive testing.
+         * solution, it is generally a safe assumption that any minima near
+         * this point is a global minima. 
          *
          * Args:
-         *   core_alloc: A map from asid to the number of cores currently
+         *   core_alloc: The core allocation from where to start gradient
+         *     descent. It is a map from asid to the number of cores currently
          *     allocated.  When the function returns, the updated core
          *     allocations are stored in this map.
          *   process_scaling: A vector of linear scaling
@@ -88,22 +93,75 @@ class BaseSpeedupModel {
          *   process_serial_runtime: A vector of serial runtimes for
          *     each process, where the index of the element equals the asid of
          *     the process.
-         *   prev_max: The worst-case runtime from the previous
-         *     invocation of this function. This is used because this function
-         *     is recursive. This equals -1 if the current worst-case runtime is
+         *
+         * Returns: The minimum energy of the system, with the corresponding
+         *   minimum core allocation stored in @core_alloc.
+         */
+        double MinimizeCoreAllocations(
+                std::map<int, int> &core_alloc,
+                std::vector<double> &process_scaling,
+                std::vector<double> &process_serial_runtime);
+
+        /* Returns the current number of cores allocated in @core_alloc.
+         *
+         * Args:
+         *   core_alloc: Map from asid to number of cores allocated for that
+         *     process.
+         *
+         * Returns:
+         *   The total number of cores allocated.
+         */
+        int ComputeCurrentCoresAllocated(std::map<int, int> &core_alloc);
+
+        /* Computes the energy a system would consume under a given core
+         * allocation and process scaling behaviors.
+         *
+         * Args:
+         *   process_scaling: A vector of linear scaling
+         *     factors, where the index of the element equals the asid of the
+         *     process.
+         *   process_serial_runtime: A vector of serial runtimes for
+         *     each process, where the index of the element equals the asid of
+         *     the process.
+         *
+         * Returns:
+         *   The energy consumed by the system.
+         */
+        double ComputeEnergy(
+                std::map<int, int> &core_alloc,
+                std::vector<double> &process_scaling,
+                std::vector<double> &process_serial_runtime);
+
+    private:
+        /* Performs gradient descent recursively. This is the function that
+         * does the true work; the other overloaded function is a convenience
+         * method.
+         *
+         * Additional args:
+         *   process_list: A vector of asids. Each recursion of this function
+         *     explores adding, removing, or keeping constant a core to a
+         *     process. When a process's allocation is tentatively modified,
+         *     that process's asid is removed from this list. The function
+         *     returns when this list is empty, thus guaranteeing convergence.
+         *   previous_max: The worst-case runtime from the previous invocation
+         *     of this function. This is used because this function is
+         *     recursive. This equals -1 if the current worst-case runtime is
          *     worse than the previous maximum (which means the current
          *     allocation is less optimal). The first invocation of this
          *     function sets this parameter to -1.
          *
-         * Returns: The maximum runtime of the system under the current core
-         *     allocation (stored in @core_alloc) if the current maximum runtime
-         *     is less than the previous maximum, or -1 otherwise.
+         * Returns:
+         *   The minimum energy point found from the provided starting point.
+         *   The allocation corresponding to this minimum energy is stored in
+         *   @core_alloc.
          */
         double MinimizeCoreAllocations(
                 std::map<int, int> &core_alloc,
                 std::vector<double> &process_scaling,
                 std::vector<double> &process_serial_runtime,
-                double prev_max);
+                std::vector<int> &process_list,
+                double previous_max);
+
 };
 
 #endif
