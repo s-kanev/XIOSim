09c0ea970d0d6eec5f156b0d1095094310ee163d
Svilen Kanev <skanev@eecs.harvard.edu>
2014-08-22 03:01:17 -0700

    Simpler sample scripts.
    Change-Id: Ia4cf64df29d49924bdae4d67fe2508116756d331

--- pintool/run.sh
+++ pintool/run.sh
@@ -1,44 +1,25 @@
-#! /bin/csh -f
+#! /bin/bash
 
-set PROGRAM = ../tests/fib
-#set PROGRAM = ../tests/fork
-#set PROGRAM = /home/skanev/ubench/fib
-#set PROGRAM = /home/skanev/cpuburn/burnP5
-#set PROGRAM = /home/skanev/misc/tests/st_test/st_test
-#set PROGRAM = /home/skanev/pfmwrapper/main
-#set PINHOME = /home/skanev/pin/pin-2.10-45467-gcc.3.4.6-ia32_intel64-linux
-#set PIN = "$PINHOME/ia32/bin/pinbin"
-set PINHOME = /home/skanev/pin/2.12
-set PIN = "$PINHOME/pin.sh"
-set PINTOOL = ./obj-ia32/feeder_zesto.so
-set ZESTOCFG = ../config/A.cfg
-#set MEMCFG = ../dram-config/DDR2-800-5-5-5.cfg
-#set ZESTOCFG = ../config/N.cfg
-set MEMCFG = ../dram-config/DDR3-1600-9-9-9.cfg
-set MAX = 1000000
-set BENCHMARK_CFG_FILE = "benchmarks.cfg"
-#set MAX = -1
+PROGRAM=../tests/fib
+PINHOME=/home/skanev/pin/2.12
+PIN=$PINHOME/pin.sh
+PINTOOL=./obj-ia32/feeder_zesto.so
+ZESTOCFG=../config/Nconfuse.cfg
+MEMCFG=../dram-config/DDR3-1600-9-9-9.cfg
+BENCHMARK_CFG_FILE=benchmarks.cfg
 
-setenv LD_LIBRARY_PATH "/home/skanev/lib"
+export LD_LIBRARY_PATH=/home/skanev/lib
 
-set CMD_LINE = "setarch i686 -BR ./obj-ia32/harness \
+CMD_LINE="setarch i686 -BR ./obj-ia32/harness \
                 -benchmark_cfg $BENCHMARK_CFG_FILE  \
                 -pin $PIN  \
                 -pause_tool 1  \
                 -xyzzy  \
                 -t  \
                 $PINTOOL  \
-                -num_cores 2  \
+                -num_cores 1  \
                 -s  \
-                -config $ZESTOCFG  \
-                -config $MEMCFG  \
-                -cores 2  \
-                -allocator gang:2 \
-                -redir:sim tst.out  \
-                -heartbeat 10000"
-#-power true -power:rtp_interval 10000 -power:rtp_file tst.power -dvfs:interval 40000 -- $PROGRAM"
+                -config $ZESTOCFG"
 
 echo $CMD_LINE
 $CMD_LINE
-
-#/usr/bin/env -i setarch i686 -3BL /home/skanev/pin/pin-2.8-36111-gcc.3.4.6-ia32_intel64-linux/ia32/bin/pinbin -pause_tool 1 -separate_memory -t /home/skanev/Zesto/pintool/obj-ia32/feeder_zesto.so -ppfile fib..pintool.1.pp -s -config /home/skanev/zesto/config/A.cfg -config /home/skanev/zesto/dram-config/DDR2-533-4-4-4.cfg -redir:sim tst.out -- /home/skanev/ubench/fib
