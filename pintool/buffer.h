766e822d72cc189aa185b7f99608b8dfcd029a40
Svilen Kanev <skanev@eecs.harvard.edu>
2015-07-23 14:00:26 -0400

    Move Buffer to header-only.
    This way its hot functions can be inlined.
    Also, remove unnecessary divide in Buffer::get_item().
    
    Change-Id: I57795c5d2a8f18b583111c5403cce42c243304ab

--- pintool/buffer.h
+++ pintool/buffer.h
@@ -1,6 +1,7 @@
 #ifndef BUF_FER_H
 #define BUF_FER_H
 
+#include <cstddef>
 #include <map>
 #include <queue>
 #include <assert.h>
@@ -8,37 +9,82 @@
 
 class Buffer {
   public:
-    Buffer(int size);
-    Buffer();
+    Buffer(int size)
+        : numPool_(size)
+        , handshakePool_(new handshake_container_t[numPool_])
+        , head_(0)
+        , tail_(0)
+        , size_(0) {}
 
     // Push is split in two phases: (i) a non-destructive get_buffer(),
     // which returns a pointer to an internal storage element and (ii)
     // push_done(), which markes the element as unavailable and occupying
     // space.
-    handshake_container_t* get_buffer();
-    void push_done();
+    handshake_container_t* get_buffer() {
+        assert(!full());
+        return &(handshakePool_[head_]);
+    }
 
-    void pop();
+    void push_done() {
+        assert(!full());
 
-    handshake_container_t* front();
-    handshake_container_t* back();
+        head_++;
+        if (head_ == numPool_) {
+            head_ = 0;
+        }
+        size_++;
+        assert(size_ <= numPool_);
+    }
 
-    handshake_container_t* get_item(int index);
-    handshake_container_t* operator[](int index);
+    void pop() {
+        handshake_container_t* handshake = front();
+        handshake->Clear();
 
-    bool empty();
-    bool full();
+        tail_++;
+        if (tail_ == numPool_) {
+            tail_ = 0;
+        }
+        size_--;
+    }
 
-    int size();
-    int capacity();
+    handshake_container_t* front() {
+        assert(size_ > 0);
+        return &(handshakePool_[tail_]);
+    }
+
+    handshake_container_t* back() {
+        assert(size_ > 0);
+
+        int dex = (head_ - 1);
+        if (dex == -1) {
+            dex = numPool_ - 1;
+        }
+        return &(handshakePool_[dex]);
+    }
+
+    // [0] corresponds to front(), [size_ - 1] to back()
+    // does not bounds check
+    handshake_container_t* get_item(int index) {
+        int dex = (tail_ + index);
+        if (dex >= numPool_)
+            dex -= numPool_;
+        return &(handshakePool_[dex]);
+    }
+    handshake_container_t* operator[](int index) { return get_item(index); }
+
+    bool empty() { return size_ == 0; }
+    bool full() { return size_ == numPool_; }
+
+    int size() { return size_; }
+    int capacity() { return numPool_; }
 
   private:
+    int numPool_;
     handshake_container_t* handshakePool_;
     int head_;
     int tail_;
 
     int size_;
-    int numPool_;
 };
 
 #endif
