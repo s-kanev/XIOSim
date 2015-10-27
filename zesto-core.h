39f8afaa250ca134406d7dd12771caa903c5944b
Svilen Kanev <skanev@eecs.harvard.edu>
2015-10-27 14:13:06 -0700

    Removed SimpleScalar license.
    No code derived from SimpleScalar left.
    
    Change-Id: I763a9265abe47112471064890a6a983c234acd9c

--- zesto-core.h
+++ zesto-core.h
@@ -74,66 +74,6 @@
  * 7. Zesto was developed by Gabriel H. Loh, Ph.D.  US Mail: 266 Ferst Drive,
  * Georgia Institute of Technology, Atlanta, GA 30332-0765
  *
- * NOTE: This file (zesto-oracle.c) contains code directly and
- * indirectly derived from previous SimpleScalar source files.
- * These sections are demarkated with "<SIMPLESCALAR>" and
- * "</SIMPLESCALAR>" to specify the start and end, respectively, of
- * such source code.  Such code is bound by the combination of terms
- * and agreements from both Zesto and SimpleScalar.  In case of any
- * conflicting terms (for example, but not limited to, use by
- * commercial entities), the more restrictive terms shall take
- * precedence (e.g., commercial and for-profit entities may not
- * make use of the code without a license from SimpleScalar, LLC).
- * The SimpleScalar terms and agreements are replicated below as per
- * their original requirements.
- *
- * SimpleScalar Ô Tool Suite
- * © 1994-2003 Todd M. Austin, Ph.D. and SimpleScalar, LLC
- * All Rights Reserved.
- * 
- * THIS IS A LEGAL DOCUMENT BY DOWNLOADING SIMPLESCALAR, YOU ARE AGREEING TO
- * THESE TERMS AND CONDITIONS.
- * 
- * No portion of this work may be used by any commercial entity, or for any
- * commercial purpose, without the prior, written permission of SimpleScalar,
- * LLC (info@simplescalar.com). Nonprofit and noncommercial use is permitted as
- * described below.
- * 
- * 1. SimpleScalar is provided AS IS, with no warranty of any kind, express or
- * implied. The user of the program accepts full responsibility for the
- * application of the program and the use of any results.
- * 
- * 2. Nonprofit and noncommercial use is encouraged.  SimpleScalar may be
- * downloaded, compiled, executed, copied, and modified solely for nonprofit,
- * educational, noncommercial research, and noncommercial scholarship purposes
- * provided that this notice in its entirety accompanies all copies. Copies of
- * the modified software can be delivered to persons who use it solely for
- * nonprofit, educational, noncommercial research, and noncommercial
- * scholarship purposes provided that this notice in its entirety accompanies
- * all copies.
- * 
- * 3. ALL COMMERCIAL USE, AND ALL USE BY FOR PROFIT ENTITIES, IS EXPRESSLY
- * PROHIBITED WITHOUT A LICENSE FROM SIMPLESCALAR, LLC (info@simplescalar.com).
- * 
- * 4. No nonprofit user may place any restrictions on the use of this software,
- * including as modified by the user, by any other authorized user.
- * 
- * 5. Noncommercial and nonprofit users may distribute copies of SimpleScalar
- * in compiled or executable form as set forth in Section 2, provided that
- * either: (A) it is accompanied by the corresponding machine-readable source
- * code, or (B) it is accompanied by a written offer, with no time limit, to
- * give anyone a machine-readable copy of the corresponding source code in
- * return for reimbursement of the cost of distribution. This written offer
- * must permit verbatim duplication by anyone, or (C) it is distributed by
- * someone who received only the executable form, and is accompanied by a copy
- * of the written offer of source code.
- * 
- * 6. SimpleScalar was developed by Todd M. Austin, Ph.D. The tool suite is
- * currently maintained by SimpleScalar LLC (info@simplescalar.com). US Mail:
- * 2395 Timbercrest Court, Ann Arbor, MI 48105.
- * 
- * Copyright © 1994-2003 by Todd M. Austin, Ph.D. and SimpleScalar, LLC.
- *
  */
 
 #include "stats.h"
